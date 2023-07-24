d14d8e260c670919802d87e582ab36b9b36e3682
artjom <artemdobrom@gmail.com>
2023-07-24 19:00:27 +0300

    Home work done

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,81 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        task0();
+        task1();
+        task2();
+        task3();
     }
-}
+
+    public static void task0() {
+        System.out.println("Строка. Образец");
+
+        String phone = "+72-58-98 29-55";
+        phone = phone.replace("-","" );
+        phone = phone.replace("+","" );
+        phone = phone.replace(" ", "");
+        if (phone.length()==10) {
+//            phone = '3' + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Телофон слишком короткий");
+        } else if (phone.length() == 11 && phone.charAt(0) != '3') {
+            throw new RuntimeException("Иностранный номер");
+        }
+        System.out.println("phone = " + phone);
+        String expextedPhone = "37258982955";
+        if (phone.equals(expextedPhone)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
+        }
+
+        public static void task1() {
+            System.out.println();
+            System.out.println("Строка.Задание 1");
+
+            String firstName = "Иван ";
+            String middleName = "Иванович ";
+            String lastName = "Иванов ";
+            String fullName = lastName + firstName + middleName;
+//            String fullName = String.join(" ", lastName + firstName + middleName);
+            System.out.println("ФИО сотрудника- " + fullName);
+        }
+
+
+    public static void task2() {
+        System.out.println();
+        System.out.println("Строка.Задание 2");
+
+        String firstName = "Иван ";
+        String middleName = "Иванович ";
+        String lastName = "Иванов ";
+        String fullName = lastName + firstName + middleName;
+//        fullName = fullName.replace("Иванов Иван Иванович ","ИВАНОВ ИВАН ИВАНОВИЧ " );
+        System.out.println("Данные ФИО сотрудников для заполнения отчёта-  " + fullName.toUpperCase());
+//        System.out.println("Данные ФИО сотрудников для заполнения отчёта-  " + fullName);
+
+    }
+
+
+    public static void task3() {
+        System.out.println();
+        System.out.println("Строка.Задание 3");
+
+        String firstName = "Семён ";
+        String middleName = "Семёнович ";
+        String lastName = "Иванов ";
+
+        String fullName = lastName + firstName + middleName;
+
+        fullName = fullName.replace("ё","е" );
+
+        System.out.println("Данные ФИО сотрудника-  " + fullName.toUpperCase());
+
+
+
+    }
+    }
